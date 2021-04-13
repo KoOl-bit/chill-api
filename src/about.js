30b09f5e71af7032dccdf0e6028ab3fc7454aff6
JustDoIt <JustDoIt@Olivers-MBP.lan>
2021-04-13 14:30:16 +0200

    chill and grill

--- src/about.js
+++ src/about.js
@@ -3,7 +3,7 @@ import React from 'react'
 const about = () => {
     return (
         <div className="page-heading"><h1>Creators</h1>
-        <p>This is a website of the Masters of Disasters</p>
+        <h4>This is a website of the Masters of Disasters</h4>
         </div>
             
 
